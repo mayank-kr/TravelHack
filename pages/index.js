cfa68ef3e6b9bf9b228fe28de57398d44bde9b21
Mayank Kumar <kmayank38@gmail.com>
2021-06-30 00:44:56 +0530

    Updated title and footer, removed placeholder cards

--- pages/index.js
+++ pages/index.js
@@ -4,59 +4,20 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Travel Hack</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://mlh.io">Major League Hacking!</a>
+          Welcome to <a href="https://mlh.io">Travel Hack!</a>
         </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
+        <span>
+          Powered by{' '}<a href="http://github.com/mayank-kr/" target="_blank" rel="noreferrer">Mayank Kumar</a>
+      </span>
       </footer>
 
       <style jsx>{`
@@ -82,13 +43,10 @@ export default function Home() {
           width: 100%;
           height: 100px;
           border-top: 1px solid #eaeaea;
+          text-align: center;
           display: flex;
-          justify-content: center;
           align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
+          justify-content: center;
         }
 
         footer a {
@@ -102,14 +60,14 @@ export default function Home() {
           text-decoration: none;
         }
 
-        .title a {
+        a {
           color: #0070f3;
           text-decoration: none;
         }
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
+        a:hover,
+        a:focus,
+        a:active {
           text-decoration: underline;
         }
 
